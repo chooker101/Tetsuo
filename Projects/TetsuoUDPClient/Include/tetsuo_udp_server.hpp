c461aa41fbabbc4b62b10682c7795ab243adc621
carsten.hooker <carsten.hooker@skyboxlabs.com>
2019-10-15 14:09:05 -0700

    Submodule add

---
+++ Projects/TetsuoUDPClient/Include/tetsuo_udp_server.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace tet
+{
+	class UdpServer
+	{
+	private:
+
+	};
+}
--- Projects/TetsuoUDPClient/Include/tetsuo_udp_server_layout.hpp
+++ Projects/TetsuoUDPClient/Include/tetsuo_udp_server_layout.hpp
@@ -11,13 +11,23 @@ namespace tet
 	class UdpServerLayout
 	{
 	private:
-		asio::io_context* mIOContext;
-		IpStandardFlag mIpFlag;
-		uint16_t mPort;
-		uint32_t mMaxDataLength;
+		asio::io_context* mIOContext = nullptr;
+		IpStandardFlag mIpFlag = IpStandardFlag::IPV4;
+		uint16_t mPort = 80;
+		uint32_t mMaxDataLength = 1024;
 	public:
 		UdpServerLayout(asio::io_context* context, uint16_t port);
 		UdpServerLayout(asio::io_context* context, IpStandardFlag ipStandard, uint16_t port);
 		UdpServerLayout(asio::io_context* context, IpStandardFlag ipStandard, uint16_t port, uint32_t maxDataLength);
+
+		const asio::io_context* GetIOContext() const { return mIOContext; }
+		IpStandardFlag GetIpFlag() const { return mIpFlag; }
+		uint16_t GetPort() const { return mPort; }
+		uint32_t GetMaxDataLength() const { return mMaxDataLength; }
+
+		void SetIOContext(asio::io_context* context) { mIOContext = context; }
+		void SetIpFlag(IpStandardFlag ipStandard) { mIpFlag = ipStandard; }
+		void SetPort(uint16_t port) { mPort = port; }
+		void SetMaxDataLength(uint32_t maxDataLength) { mMaxDataLength = maxDataLength; }
 	};
 }
