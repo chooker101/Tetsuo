fe4774a907fdec6dc207ab7b86ab4accd8a973c5
carsten.hooker <carsten.hooker@skyboxlabs.com>
2019-10-23 16:58:02 -0700

    Work on server cpp

--- Projects/TetsuoUDPClient/Include/tetsuo_udp_server.hpp
+++ Projects/TetsuoUDPClient/Include/tetsuo_udp_server.hpp
@@ -9,8 +9,12 @@ namespace tet
 		UdpServerLayout* mServerLayout = nullptr;
 		asio::ip::udp::socket mSocket;
 		asio::ip::udp::endpoint mSenderEndpoint;
+		uint8_t* mData = nullptr;
+		
+		void Recieve();
 	public:
 		UdpServer(UdpServerLayout* serverLayout);
-
+		void SetDataPointer(uint8_t* data);
+		void Send();
 	};
 }
--- Projects/TetsuoUDPClient/Source/tetsuo_udp_server.cpp
+++ Projects/TetsuoUDPClient/Source/tetsuo_udp_server.cpp
@@ -1,8 +1,21 @@
 #include <precompiled.hpp>
 #include "tetsuo_udp_server.hpp"
 
-tet::UdpServer::UdpServer(UdpServerLayout * serverLayout) : 
-	mServerLayout(serverLayout), 
+void tet::UdpServer::Recieve()
+{
+}
+
+tet::UdpServer::UdpServer(UdpServerLayout * serverLayout) :
+	mServerLayout(serverLayout),
 	mSocket(mServerLayout->GetIOContext(), asio::ip::udp::endpoint((mServerLayout->GetIpFlag() == IpStandard::IPV4 ? asio::ip::udp::v4() : asio::ip::udp::v6()), mServerLayout->GetPort()))
+{
+}
+
+void tet::UdpServer::SetDataPointer(uint8_t * data)
+{
+	mData = data;
+}
+
+void tet::UdpServer::Send()
 {
 }
